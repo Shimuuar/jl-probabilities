b7df4c382e2d7a2f83d93b063cd97c5ad8cf20b2
Batmaev <vbbatmaev@gmail.com>
2024-05-13 16:13:16 +0300

    periodogram with more points

--- mle.jl
+++ mle.jl
@@ -21,12 +21,12 @@ begin
 	using Plots
 	using StatsPlots
 	plotlyjs()
-	theme(:juno)
+	theme(:default)
 
 	using LombScargle
 end
 
-# ╔═╡ 3d70079b-add5-4b60-9cb2-af7ed5caad87
+# ╔═╡ 2408848a-e855-44cb-9b4f-3c6ced602e07
 using Dates
 
 # ╔═╡ 0a209f5e-bd22-4072-9247-c0ddcd42fdb8
@@ -40,21 +40,28 @@ begin
 end
 
 # ╔═╡ 18694e43-32c0-424f-ade0-63c6fc8af923
-pgram = lombscargle(points.day, points.K)
+pgram = lombscargle(points.day, points.K, samples_per_peak = 100)
+
+# ╔═╡ adcd2388-810f-4e3e-9901-73bf285f6731
+plotlyjs()
 
 # ╔═╡ 4b30de9a-e872-422c-9b12-2cc67942aba3
-plot(
+pgramplot = plot(
 	freq(pgram),
 	power(pgram),
 	xlabel = "частота (1/день)",
-	title = "Lomb-Scargle periodogram, сигнал K"
+	title = "Периодограмма Ломба-Скаргла",
+	legend = nothing,
+	xlims = (0.003, 0.015),
+	linewidth = 1.2,
+	size = (600, 360)
 )
 
+# ╔═╡ 49216477-bda7-487c-9c85-56e1ed842d84
+savefig(pgramplot, "tex/pic_drafts/periodogram.svg")
+
 # ╔═╡ c73161c8-0eb3-40af-b527-40a05e10ae24
-begin
-	estimated_period = 2findmaxperiod(pgram)[1]
-	findmaxperiod(pgram)
-end
+estimated_period = 2findmaxperiod(pgram)[1]
 
 # ╔═╡ a6dccb45-fa0c-4aff-ae2e-23efc08fd8da
 interpolated_mesh = InterpolatedRocheMesh(tetra_sphere(4), 0.1:0.1:10)
@@ -95,7 +102,8 @@ function plot_garbige(model_params, samples, add_σ = false)
 		xlabel = ["" "Julian day % period"],
 		ylabel = "Звездная величина",
 		yflip = true,
-		size = (600, 600)
+		size = (600, 600),
+		#bottom_margin = 22Plots.px
 	)
 
 	for (channel, subplot) ∈ zip(model_params.channels, p.subplots)
@@ -134,7 +142,7 @@ channels = [
 		measurements_y = points.K,
 		darkening_function = claret_darkening,
 		darkening_coefs_interpolant = K_coefs_interpolant,
-		luminocity_function = black_body_K,
+		luminocity_function = phoenixK,
 		σ_measured = points.K_err,
 		σ_common = FlatPos(0.),
 	)
@@ -143,18 +151,43 @@ channels = [
 		measurements_y = points.J,
 		darkening_function = claret_darkening,
 		darkening_coefs_interpolant = J_coefs_interpolant,
-		luminocity_function = black_body_J,
+		luminocity_function = phoenixJ,
 		σ_measured = points.J_err,
 		σ_common = FlatPos(0.),
 	)
 ]
 
+# ╔═╡ c92d3193-54d8-4689-9d26-0b8edbc56b81
+plot_garbige(
+	ModelParams(
+		channels = channels,
+		period = estimated_period
+	),
+	[]
+)
+
 # ╔═╡ 9d29eca2-57c2-4ace-82de-1472c5509cd8
 model_params = ModelParams(
 	channels = channels,
 	period = estimated_period,
-	β = 0.25,
+	β = 0.08,
 )
+
+# ╔═╡ 991316a4-0a9c-433b-847b-4c4a4e92a24e
+initial_params = (;
+	mass_quotient = 0.8,
+	initial_phase = -1.45,
+	observer_angle = π/2 - 0.1,
+	temperature_at_bottom = 3500.,
+	σ_common = [0.1, 0.1],
+	offset = [46.65, 48.85],
+)
+
+# ╔═╡ 76c7e96a-e138-4102-9b16-cfbe2a2b93f7
+plot_garbige(model_params, [initial_params])
+
+# ╔═╡ e18a1c5d-3271-4cfc-b8ac-fed64584d936
+estimated_period
 
 # ╔═╡ e0419433-72d5-4d12-9a81-838886dd7317
 model = first_model(model_params)
@@ -182,18 +215,14 @@ function MAP_point(model, initial_params)
 	return DynamicPPL.values_as(var_info, NamedTuple)
 end
 
-# ╔═╡ 991316a4-0a9c-433b-847b-4c4a4e92a24e
-initial_params = (;
-	mass_quotient = 0.5,
-	initial_phase = -1.45,
-	observer_angle = π/2 - 0.1,
-	temperature_at_bottom = 3500.,
-	σ_common = [0.1, 0.1],
-	offset = [18.84, 21.15],
-)
-
 # ╔═╡ f7c074e7-d5b1-4b2c-9ab6-57e85b237a04
 MAP_1 = MAP_point(model, initial_params)
+
+# ╔═╡ 9d1e69c3-f416-414c-9d22-762b7fffb783
+MAP_1[:offset]
+
+# ╔═╡ 64e1fecd-96c8-4eb5-ab21-d7593220276a
+MAP_1
 
 # ╔═╡ 3297cc6f-287f-4b30-a203-0a5add3b7ca0
 rad2deg(MAP_1[:observer_angle])
@@ -290,17 +319,24 @@ differences(model_params, MAP_1)
 # ╠═11decfb6-8f0a-11ee-1bf8-d3faf8756b8b
 # ╠═0a209f5e-bd22-4072-9247-c0ddcd42fdb8
 # ╠═18694e43-32c0-424f-ade0-63c6fc8af923
+# ╠═adcd2388-810f-4e3e-9901-73bf285f6731
 # ╠═4b30de9a-e872-422c-9b12-2cc67942aba3
+# ╠═49216477-bda7-487c-9c85-56e1ed842d84
 # ╠═c73161c8-0eb3-40af-b527-40a05e10ae24
 # ╠═a6dccb45-fa0c-4aff-ae2e-23efc08fd8da
 # ╠═b782aa83-2e9d-449c-a996-2064c890c025
 # ╠═ed6c9a39-4a07-4e98-b893-537d13b1a45d
+# ╠═c92d3193-54d8-4689-9d26-0b8edbc56b81
 # ╠═0d54214f-5b15-44cc-bce0-96829c4b5470
 # ╠═9d29eca2-57c2-4ace-82de-1472c5509cd8
+# ╠═991316a4-0a9c-433b-847b-4c4a4e92a24e
+# ╠═76c7e96a-e138-4102-9b16-cfbe2a2b93f7
+# ╠═9d1e69c3-f416-414c-9d22-762b7fffb783
+# ╠═64e1fecd-96c8-4eb5-ab21-d7593220276a
+# ╠═e18a1c5d-3271-4cfc-b8ac-fed64584d936
 # ╠═e0419433-72d5-4d12-9a81-838886dd7317
 # ╠═b1701d19-4cb7-4c35-943e-6a9cd415c4a1
 # ╠═a3b38672-7e73-4701-abc6-30d057f09754
-# ╠═991316a4-0a9c-433b-847b-4c4a4e92a24e
 # ╠═f7c074e7-d5b1-4b2c-9ab6-57e85b237a04
 # ╠═3297cc6f-287f-4b30-a203-0a5add3b7ca0
 # ╠═454c33e2-bf88-471d-b51c-6f2010433fef
@@ -311,8 +347,8 @@ differences(model_params, MAP_1)
 # ╠═bce9145d-23f2-4e22-90bc-0c0089502286
 # ╠═363ea8d3-a174-4186-a967-be659aaa9a72
 # ╟─eefb61ae-924d-4bb7-9b9a-7235b1f4b826
+# ╠═2408848a-e855-44cb-9b4f-3c6ced602e07
 # ╠═b85b3eb7-5257-43d9-9261-0736f7102474
 # ╠═c5ad427c-c144-4cc2-b531-ff830beaa21e
 # ╠═6aded0f0-c6b3-4bf2-872c-eb9b264caf00
 # ╠═360c7b42-5f18-4137-ac62-e747b47501ff
-# ╠═3d70079b-add5-4b60-9cb2-af7ed5caad87
